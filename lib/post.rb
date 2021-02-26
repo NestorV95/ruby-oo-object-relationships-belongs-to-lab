360cf2b556d4ee94e34b94b648cba0c552d039ed
Nestor <nestor.venegas95@gmail.com>
2021-02-25 22:43:37 -0600

    commit tree 80e03f35b995aa6d2c35d1d5d256d3b56a42d170

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+    attr_accessor :title, :author
+
+    # def initialize(title, author)
+    #     @title = title
+    #     @author = author
+    # end
+ 
+    def title
+     @title
+    end
+
+    def author
+        @author
+    end
+end
